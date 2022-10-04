bc07099a89edf845a300389b12373c551e7f4f92
github-action-benchmark <github@users.noreply.github.com>
2022-10-04 10:47:08 +0000

    add Benchmark (googlecpp) benchmark result for 52d0cc79dc133f5bd5288ae779d685d52884fbfe

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1664880428462,
+  "repoUrl": "https://github.com/lucabart97/benchmark_test",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "lucabart97",
+            "username": "lucabart97"
+          },
+          "committer": {
+            "name": "lucabart97",
+            "username": "lucabart97"
+          },
+          "id": "52d0cc79dc133f5bd5288ae779d685d52884fbfe",
+          "message": "ciao",
+          "timestamp": "2022-09-28T16:51:58Z",
+          "url": "https://github.com/lucabart97/benchmark_test/pull/1/commits/52d0cc79dc133f5bd5288ae779d685d52884fbfe"
+        },
+        "date": 1664880427688,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_StringCreation",
+            "value": 7.018387254995594,
+            "unit": "ns/iter",
+            "extra": "iterations: 98681614\ncpu: 7.015733447570081 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_StringCopy",
+            "value": 27.31453529218739,
+            "unit": "ns/iter",
+            "extra": "iterations: 25798415\ncpu: 27.31125923821289 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
