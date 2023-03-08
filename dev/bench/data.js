03542000999876a91214df02f0a136c0edb96911
github-action-benchmark <github@users.noreply.github.com>
2023-03-08 08:09:51 +0000

    add C++ Benchmark (googlecpp) benchmark result for ec44e52c2c351fdf6b8c6e7a6ddef0a3de8b5dfb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1664880428462,
+  "lastUpdate": 1678262991606,
   "repoUrl": "https://github.com/lucabart97/benchmark_test",
   "entries": {
     "Benchmark": [
@@ -32,6 +32,44 @@ window.BENCHMARK_DATA = {
             "value": 27.31453529218739,
             "unit": "ns/iter",
             "extra": "iterations: 25798415\ncpu: 27.31125923821289 ns\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "C++ Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "luca.bartoli@hipert.it",
+            "name": "Luca Bartoli",
+            "username": "lucabart97"
+          },
+          "committer": {
+            "email": "luca.bartoli@hipert.it",
+            "name": "Luca Bartoli",
+            "username": "lucabart97"
+          },
+          "distinct": true,
+          "id": "ec44e52c2c351fdf6b8c6e7a6ddef0a3de8b5dfb",
+          "message": "push1\n\nSigned-off-by: Luca Bartoli <luca.bartoli@hipert.it>",
+          "timestamp": "2023-03-08T09:09:13+01:00",
+          "tree_id": "6549dfe63e2fd106f38b89a899ad839601696475",
+          "url": "https://github.com/lucabart97/benchmark_test/commit/ec44e52c2c351fdf6b8c6e7a6ddef0a3de8b5dfb"
+        },
+        "date": 1678262990608,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "BM_StringCreation",
+            "value": 6.184598378293219,
+            "unit": "ns/iter",
+            "extra": "iterations: 112794252\ncpu: 6.184386062509639 ns\nthreads: 1"
+          },
+          {
+            "name": "BM_StringCopy",
+            "value": 25.781912173441746,
+            "unit": "ns/iter",
+            "extra": "iterations: 26955787\ncpu: 25.780193321753135 ns\nthreads: 1"
           }
         ]
       }
